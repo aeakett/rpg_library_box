390f7d86579159e7e14aa0252ce39680033d3b40
Andrew Eakett <aeakett@gmail.com>
2014-11-07 08:37:07 -0500

    a bunch of cleaning and prettying-up

---
+++ mythender/img/favicon/asdf.sh
@@ -0,0 +1,334 @@
+#!/bin/bash
+ 
+ convert $1 -resize 180x180 apple-touch-icon-180x180-precomposed.png
+ convert $1 -resize 152x152 apple-touch-icon-152x152-precomposed.png
+ convert $1 -resize 144x144 apple-touch-icon-144x144-precomposed.png
+ convert $1 -resize 120x120 apple-touch-icon-120x120-precomposed.png
+ convert $1 -resize 114x114 apple-touch-icon-114x114-precomposed.png
+ convert $1 -resize 76x76 apple-touch-icon-76x76-precomposed.png
+ convert $1 -resize 72x72 apple-touch-icon-72x72-precomposed.png
+ convert $1 -resize 57x57 apple-touch-icon-precomposed.png
+ convert $1 -resize 192x192 touch-icon-192x192.png
+ convert $1 -resize 70x70 metro_tile-70x70.png
+ convert $1 -resize 150x150 metro_tile-150x150.png
+ convert $1 -resize 310x310 metro_tile-310x310.png
+ echo
+ echo 'Check http://mathiasbynens.be/notes/touch-icons periodically to see if this script needs to be updated.'
+ echo "You'll probably want to optimize the resulting PNG files."
+ echo 'Also, head on over to http://xiconeditor.com/ and make yourself a nice favicon.'
+ eoy: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="180x180" href="apple-touch-icon-180x180-precomposed.png">'
+ echo '<!-- For Chrome for Android: -->'
+ echo '<link rel="icon" sizes="192x192" href="touch-icon-192x192.png">'
+ echo '<!-- Nice stuff for Metro -->'
+ echo '<!-- Add your name here -->'
+ echo '<meta name="application-name" content="Name Goes Here"/>'
+ echo '<!-- Set a nice background colour for your tile -->'
+ echo '<meta name="msapplication-TileColor" content="#f357a1"/>'
+ echo '<meta name="msapplication-square70x70logo" content="metro_tile-70x70.png"/>'
+ echo '<meta name="msapplication-square150x150logo" content="metro_tile-150x150.png"/>'
+ echo "<!-- You'll need to make this one on your own -->"
+ echo '<!-- <meta name="msapplication-wide310x150logo" content="metro_tile-310x150.png"/> -->'
+ echo '<meta name="msapplication-square310x310logo" content="metro_tile-310x310.png"/>'y running iOS  7: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152-precomposed.png">'
+ echo '<!-- For iPhone 6 Plus with @3y running iOS  6: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png">'
+ echo '<!-- For iPad with @2y running iOS  7: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120-precomposed.png">'
+ echo '<!-- For iPad with @2y running iOS  6: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png">'
+ echo '<!-- For iPhone with @2y) on iOS  7: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76-precomposed.png">'
+ echo '<!-- For iPhone with @2y) on iOS  6: -->'
+ echo '<link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png">'
+ echo '<!-- For the iPad mini and the first- and second-generation iPad (@1'<!-- For the iPad mini and the first- and second-generation iPad (@1y) iPhone, iPod Touch, and Android 2.1+ devices: -->'
+ echo '<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png"><!-- 57ho
+ cho
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
+ echo
+ echo 'Copy and paste the following into your HTML <head>:'
+ echo
+ echo '<!-- For non-Retina (@1
